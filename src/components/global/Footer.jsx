4922ee7d5531901ac7b1e94d1c92e9a3a600bbf4
Peace Garden <peacegarden@Peaces-MacBook-Air.local>
2023-06-06 12:10:30 +0530

    footer modifed

--- src/components/global/Footer.jsx
+++ src/components/global/Footer.jsx
@@ -1,38 +1,45 @@
 'use client'
 import React from 'react';
-import { Box, HStack, Spacer, Stack, Text, VStack, Image } from '@chakra-ui/react';
-import { BsFacebook, BsInstagram, BsLinkedin, BsPhone, BsTelephone, BsTelephoneFill, BsTwitter } from 'react-icons/bs'
+import { Box, HStack, Image, Spacer, Stack, Text, VStack } from '@chakra-ui/react';
+import { BsFacebook, BsInstagram, BsLinkedin, BsTelephoneFill, BsTwitter } from 'react-icons/bs'
 import { GrLocation } from 'react-icons/gr'
 
 const Footer = () => {
   return (
-    <Box p={[8, 16]} bgColor={'blanchedalmond'}>
-      <Stack direction={['column', 'row']} alignItems={'flex-end'} justifyContent={'space-between'} gap={8}>
-        <Box flex={1}>
-          <Image src='/logo.png' w={20} />
-          <Box>
-            <br />
-            <Text className='serif' pb={2} >Terms of Service</Text>
-            <Text className='serif' pb={2} >Privacy Policy</Text>
-            <Text className='serif' pb={2} >Refund Policy</Text>
-          </Box>
+    <Box w={'full'} p={8} bgColor={'blanchedalmond'}>
+      <Stack direction={['column', 'row']} gap={8}>
+        <Box w={'full'} flex={1}>
+          <Image src='/logo.png' width={24} />
+          <br />
+          <VStack w={'full'} alignItems={'flex-start'} justifyContent={'flex-start'} gap={2} flex={1}>
+            <Text fontWeight={'normal'}>Raise Funds</Text>
+            <Text fontWeight={'normal'}>Investment Opportunities</Text>
+            <Text fontWeight={'normal'}>Payment Process</Text>
+          </VStack>
         </Box>
         <Spacer />
-        <Box>
-        <HStack p={8}>
-          <GrLocation color='#333' fontSize={36} />
-          <Text fontSize={'lg'}>Space for Address</Text>
-        </HStack>
-        <HStack p={8}>
-          <BsTelephoneFill color='#333' fontSize={36} />
-          <Text fontSize={'lg'}>+91 90228 53554</Text>
-        </HStack>
-        <HStack p={8} spacing={8}>
-          <BsFacebook color='#333' fontSize={36} cursor={'pointer'} />
-          <BsInstagram color='#333' fontSize={36} cursor={'pointer'} />
-          <BsTwitter color='#333' fontSize={36} cursor={'pointer'} />
-          <BsLinkedin color='#333' fontSize={36} cursor={'pointer'} />
-        </HStack>
+        <VStack w={'full'} alignItems={'flex-start'} justifyContent={'flex-start'} gap={2} flex={1}>
+          <Text fontSize={'lg'} className='serif' fontWeight={'semibold'}>Important</Text>
+          <Text fontWeight={'normal'}>Terms & Conditions</Text>
+          <Text fontWeight={'normal'}>Privacy Policy</Text>
+          <Text fontWeight={'normal'}>Refund Policy</Text>
+        </VStack>
+        <Spacer />
+        <Box flex={1} w={'full'}>
+          <HStack p={8}>
+            <GrLocation color='#333' fontSize={36} />
+            <Text fontSize={'lg'}>Space for Address</Text>
+          </HStack>
+          <HStack p={8}>
+            <BsTelephoneFill color='#333' fontSize={36} />
+            <Text fontSize={'lg'}>+91 90228 53554</Text>
+          </HStack>
+          <HStack p={8} spacing={8}>
+            <BsFacebook color='#333' fontSize={36} cursor={'pointer'} />
+            <BsInstagram color='#333' fontSize={36} cursor={'pointer'} />
+            <BsTwitter color='#333' fontSize={36} cursor={'pointer'} />
+            <BsLinkedin color='#333' fontSize={36} cursor={'pointer'} />
+          </HStack>
         </Box>
       </Stack>
     </Box>
