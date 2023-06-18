c2c134487a4f18c0e64a35c78d6b850fd8bdb0d9
Peace Garden <peacegarden@Peaces-MacBook-Air.local>
2023-06-18 11:32:49 +0530

    campaigns added

--- src/app/campaigns/page.jsx
+++ src/app/campaigns/page.jsx
@@ -1,77 +1,55 @@
 'use client'
-import React from 'react'
-import { Box, Stack, Text } from '@chakra-ui/react'
+import React, { useEffect, useState } from 'react'
+import { Box, Stack, Text, Button, useToast } from '@chakra-ui/react'
 import CampaignCard from '@/components/campaign/CampaignCard'
+import { BsPlus } from 'react-icons/bs'
+import Link from 'next/link'
+import BackendAxios from '@/utils/axios'
 
 const Page = () => {
-    const dummyCampaigns = [
-        {
-            id: '1',
-            title: 'save our cows',
-            userName: 'john doe',
-            coverImage: 'https://t3.ftcdn.net/jpg/04/19/34/24/360_F_419342418_pBHSf17ZBQn77E7z3OWcXrWfCuxZkc3Q.jpg',
-            description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore illo quos facere asperiores. Nulla harum unde praesentium hic aut nisi iusto recusandae, beatae aspernatur.'
-        },
-        {
-            id: '2',
-            title: 'help me heal',
-            userName: 'john doe',
-            coverImage: 'https://www.hippo.co.za/globalassets/images/blog/your-guide-to-switching-medical-aid/medical-aid-2.jpg',
-            description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore illo quos facere asperiores. Nulla harum unde praesentium hic aut nisi iusto recusandae, beatae aspernatur.'
-        },
-        {
-            id: '3',
-            title: 'save your future',
-            userName: 'john doe',
-            coverImage: 'https://www.hippo.co.za/globalassets/images/blog/your-guide-to-switching-medical-aid/medical-aid-2.jpg',
-            description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore illo quos facere asperiores. Nulla harum unde praesentium hic aut nisi iusto recusandae, beatae aspernatur.'
-        },
-        {
-            id: '4',
-            title: 'education funds',
-            userName: 'john doe',
-            coverImage: 'https://www.hippo.co.za/globalassets/images/blog/your-guide-to-switching-medical-aid/medical-aid-2.jpg',
-            description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore illo quos facere asperiores. Nulla harum unde praesentium hic aut nisi iusto recusandae, beatae aspernatur.'
-        },
-        {
-            id: '5',
-            title: 'startup funds',
-            userName: 'john doe',
-            coverImage: 'https://www.hippo.co.za/globalassets/images/blog/your-guide-to-switching-medical-aid/medical-aid-2.jpg',
-            description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore illo quos facere asperiores. Nulla harum unde praesentium hic aut nisi iusto recusandae, beatae aspernatur.'
-        },
-        {
-            id: '6',
-            title: 'travel funds',
-            userName: 'john doe',
-            coverImage: 'https://www.hippo.co.za/globalassets/images/blog/your-guide-to-switching-medical-aid/medical-aid-2.jpg',
-            description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore illo quos facere asperiores. Nulla harum unde praesentium hic aut nisi iusto recusandae, beatae aspernatur.'
-        },
-    ]
+    const Toast = useToast({ position: 'top-right' })
+    const [campaigns, setCampaigns] = useState([])
+    useEffect(() => {
+        BackendAxios.get("/api/campaigns").then(res => {
+            setCampaigns(res.data)
+        }).catch(err => {
+            Toast({
+                status: 'error',
+                description: err?.response?.data?.message || err?.response?.data || err?.message
+            })
+        })
+    }, [])
 
     return (
         <>
-            <Box minH={'100vh'} p={[8, 16, 24]}>
+            <Stack direction={'row'} justifyContent={'space-between'}>
                 <Text
                     className='serif' fontSize={'xl'}
                     fontWeight={'semibold'} mb={12}
-                >Browse Social Campaigns</Text>
-                <Stack
-                    direction={['column', 'row']} flexWrap={'wrap'}
-                    gap={[8, 4, 16]} justifyContent={'flex-start'}
-                >
-                    {
-                        dummyCampaigns.map((campaign, key) => (
-                            <CampaignCard
-                                coverImage={campaign.coverImage}
-                                title={campaign.title}
-                                userName={campaign.userName}
-                                description={campaign.description}
-                            />
-                        ))
-                    }
-                </Stack>
-            </Box>
+                >Your Campaigns</Text>
+                <Link href={'/dashboard/campaigns/create'}>
+                    <Button colorScheme='yellow' rounded={'full'} leftIcon={<BsPlus size={24} />}>
+                        Create New
+                    </Button>
+                </Link>
+            </Stack>
+            <Stack
+                direction={['column', 'row']} flexWrap={'wrap'}
+                gap={[4, 8, 16]} justifyContent={'flex-start'}
+            >
+                {
+                    campaigns.filter(item => item?.status).map((campaign, key) => (
+                        <CampaignCard
+                            key={key}
+                            coverImage={campaign.file_path ? `${process.env.NEXT_PUBLIC_BACKEND_URL}/storage/${campaign.file_path}` : "https://idea.batumi.ge/files/default.jpg"}
+                            title={campaign.title}
+                            userName={campaign.status ? "Published" : "In Review"}
+                            description={campaign.description}
+                            link={`/campaigns/${campaign.id}`}
+                        />
+                    ))
+                }
+            </Stack>
         </>
     )
 }
--- src/app/dashboard/campaigns/page.jsx
+++ src/app/dashboard/campaigns/page.jsx
@@ -8,51 +8,6 @@ import BackendAxios from '@/utils/axios'
 
 const Page = () => {
     const Toast = useToast({ position: 'top-right' })
-    const dummyCampaigns = [
-        {
-            id: '1',
-            title: 'save our cows',
-            userName: 'john doe',
-            coverImage: 'https://t3.ftcdn.net/jpg/04/19/34/24/360_F_419342418_pBHSf17ZBQn77E7z3OWcXrWfCuxZkc3Q.jpg',
-            description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore illo quos facere asperiores. Nulla harum unde praesentium hic aut nisi iusto recusandae, beatae aspernatur.'
-        },
-        {
-            id: '2',
-            title: 'help me heal',
-            userName: 'john doe',
-            coverImage: 'https://www.hippo.co.za/globalassets/images/blog/your-guide-to-switching-medical-aid/medical-aid-2.jpg',
-            description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore illo quos facere asperiores. Nulla harum unde praesentium hic aut nisi iusto recusandae, beatae aspernatur.'
-        },
-        {
-            id: '3',
-            title: 'save your future',
-            userName: 'john doe',
-            coverImage: 'https://www.hippo.co.za/globalassets/images/blog/your-guide-to-switching-medical-aid/medical-aid-2.jpg',
-            description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore illo quos facere asperiores. Nulla harum unde praesentium hic aut nisi iusto recusandae, beatae aspernatur.'
-        },
-        {
-            id: '4',
-            title: 'education funds',
-            userName: 'john doe',
-            coverImage: 'https://www.hippo.co.za/globalassets/images/blog/your-guide-to-switching-medical-aid/medical-aid-2.jpg',
-            description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore illo quos facere asperiores. Nulla harum unde praesentium hic aut nisi iusto recusandae, beatae aspernatur.'
-        },
-        {
-            id: '5',
-            title: 'startup funds',
-            userName: 'john doe',
-            coverImage: 'https://www.hippo.co.za/globalassets/images/blog/your-guide-to-switching-medical-aid/medical-aid-2.jpg',
-            description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore illo quos facere asperiores. Nulla harum unde praesentium hic aut nisi iusto recusandae, beatae aspernatur.'
-        },
-        {
-            id: '6',
-            title: 'travel funds',
-            userName: 'john doe',
-            coverImage: 'https://www.hippo.co.za/globalassets/images/blog/your-guide-to-switching-medical-aid/medical-aid-2.jpg',
-            description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore illo quos facere asperiores. Nulla harum unde praesentium hic aut nisi iusto recusandae, beatae aspernatur.'
-        },
-    ]
-
     const [campaigns, setCampaigns] = useState([])
     useEffect(() => {
         BackendAxios.get("/api/user-campaigns").then(res => {
