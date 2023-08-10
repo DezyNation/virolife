97d8edf19236a35bcb3adc40e65616d2289f5942
DezyNation <87603422+DezyNation@users.noreply.github.com>
2023-08-10 20:41:32 +0530

    navbar fix

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,15 @@
-'use client'
-import Carousel from "@/components/home/Carousel"
-import AllCampaigns from "./campaigns/page"
-import Footer from "@/components/global/Footer"
+"use client";
+import Carousel from "@/components/home/Carousel";
+import AllCampaigns from "./campaigns/page";
+import Footer from "@/components/global/Footer";
+import Navbar from "@/components/global/Navbar";
 
 export default function Home() {
   return (
     <>
+      <Navbar />
       <Carousel />
-      <AllCampaigns />
+      <AllCampaigns showNavbar={false} />
     </>
-  )
+  );
 }
