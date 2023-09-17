7d1a5366eac13f46a0a330b253a825c398e7c4b6
DezyNation <87603422+DezyNation@users.noreply.github.com>
2023-09-17 22:03:33 +0530

    src

--- src/components/global/VideoPlayer.jsx
+++ src/components/global/VideoPlayer.jsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import {
-    Box,
+  Box,
   Button,
   Modal,
   ModalBody,
@@ -9,6 +9,7 @@ import {
   ModalFooter,
   ModalHeader,
   ModalOverlay,
+  Text,
   useToast,
 } from "@chakra-ui/react";
 import BackendAxios from "@/utils/axios";
@@ -20,11 +21,12 @@ const VideoPlayer = ({ title, onVideoClose, status, videoId, provider }) => {
   const Toast = useToast({ position: "top-right" });
   const [isDisabled, setIsDisabled] = useState(true);
   const [loading, setLoading] = useState(true);
+  const [link, setLink] = useState("");
   const [videoProps, setVideoProps] = useState({
     type: "video",
     sources: [
       {
-        src: videoId,
+        src: "",
         provider: provider || "youtube",
       },
     ],
@@ -32,8 +34,10 @@ const VideoPlayer = ({ title, onVideoClose, status, videoId, provider }) => {
 
   // Function Get Random Video
   const getRandomVideo = async () => {
+    setLoading(true);
     BackendAxios.get(`/api/random-video`)
       .then((res) => {
+        setLoading(false);
         setVideoProps({
           type: "video",
           sources: [
@@ -41,10 +45,12 @@ const VideoPlayer = ({ title, onVideoClose, status, videoId, provider }) => {
               src: res.data?.video_id,
               provider: "youtube",
             },
-          ]
+          ],
         });
+        setLink(res.data?.link);
       })
       .catch((err) => {
+        setLoading(false);
         Toast({
           status: "error",
           title: "Error while fetching video",
@@ -55,11 +61,11 @@ const VideoPlayer = ({ title, onVideoClose, status, videoId, provider }) => {
   };
 
   useEffect(() => {
-    getRandomVideo()
+    getRandomVideo();
   }, []);
 
   const getRandomAdSlotId = () => {
-    const adSlots = ['5151821226', '5151821226', '5151821226'];
+    const adSlots = ["5151821226", "5151821226", "5151821226"];
     const randomIndex = Math.floor(Math.random() * adSlots.length);
     return adSlots[randomIndex];
   };
@@ -74,16 +80,42 @@ const VideoPlayer = ({ title, onVideoClose, status, videoId, provider }) => {
         size={["full", "4xl"]}
       >
         <ModalOverlay />
-        <ModalContent bgColor={'transparent'} boxShadow={'none'}>
-          <ModalHeader bgColor={'#FFF'} rounded={'full'} py={2}>{title || "Watch this video to proceed."}</ModalHeader>
+        <ModalContent bgColor={"transparent"} boxShadow={"none"}>
+          <ModalHeader bgColor={"#FFF"} rounded={"full"} py={2}>
+            {title || "Watch this video to proceed."}
+          </ModalHeader>
           <ModalBody>
-            <Box rounded={'16'} overflow={'hidden'}>
-            <Plyr source={videoProps} options={{ ratio: "16:9" }} />
-            {/* <AdSenseVideoAd adSlot={getRandomAdSlotId()} /> */}
+            <Box rounded={"16"} overflow={"hidden"}>
+              {loading ? (
+                <Text color={"#FFF"} textAlign={"center"}>
+                  Please wait while we load an ad for you.
+                </Text>
+              ) : (
+                <Box
+                  w={["full", "70vw", "80vw"]}
+                  height={["480px", "560px"]}
+                  rounded={16}
+                  overflow={"hidden"}
+                >
+                  <iframe
+                    src={link}
+                    height={"100%"}
+                    width={"100%"}
+                    style={{ width: "100%", height: "100%", border: "none" }}
+                  ></iframe>
+                </Box>
+              )}
+              {/* <AdSenseVideoAd adSlot={getRandomAdSlotId()} /> */}
             </Box>
           </ModalBody>
           <ModalFooter justifyContent={"flex-end"}>
-            <Button onClick={onVideoClose} colorScheme="yellow" rounded={'full'}>Close</Button>
+            <Button
+              onClick={onVideoClose}
+              colorScheme="yellow"
+              rounded={"full"}
+            >
+              Close
+            </Button>
           </ModalFooter>
         </ModalContent>
       </Modal>
