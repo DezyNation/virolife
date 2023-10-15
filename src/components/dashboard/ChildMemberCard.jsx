dc1782b2610100bf602995548ed988e63ae5165a
DezyNation <87603422+DezyNation@users.noreply.github.com>
2023-10-16 01:31:30 +0530

    junior donation btn fix

--- src/components/dashboard/ChildMemberCard.jsx
+++ src/components/dashboard/ChildMemberCard.jsx
@@ -6,7 +6,7 @@ const ChildMemberCard = ({ name, id, phone_number, donation_received }) => {
   return (
     <>
       <HStack py={4} w={["full", "xs"]} justifyContent={"space-between"}>
-        <HStack alignItems={'flex-start'}>
+        <HStack alignItems={"flex-start"}>
           <Avatar name={name} />
           <Box w={"full"}>
             <Text className="serif" fontWeight={"semibold"} fontSize={"lg"}>
@@ -14,14 +14,16 @@ const ChildMemberCard = ({ name, id, phone_number, donation_received }) => {
             </Text>
             <Text fontSize={"xs"}>
               ID: {process.env.NEXT_PUBLIC_CODE}
-              {id} &nbsp; | &nbsp; Phone: {phone_number?.slice(0,9)}
+              {id} &nbsp; | &nbsp; Phone: {phone_number?.slice(0, 9)}
             </Text>
           </Box>
-            {donation_received ? null : (
-              <Button size={"xs"} variant={"ghost"} colorScheme="yellow">
-                Donated
-              </Button>
-            )}
+          <Button
+            size={"xs"}
+            variant={donation_received ? "ghost" : "solid"}
+            colorScheme="yellow"
+          >
+            {donation_received ? "Donated" : "Donate"}
+          </Button>
         </HStack>
       </HStack>
     </>
