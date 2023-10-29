a881b72896d2c9eea381b8b055b3cce4f196f463
DezyNation <87603422+DezyNation@users.noreply.github.com>
2023-10-29 13:06:15 +0530

    product name

--- src/app/dashboard/orders/page.jsx
+++ src/app/dashboard/orders/page.jsx
@@ -50,7 +50,7 @@ const page = () => {
             {data?.map((data, key) => (
               <Tr key={key}>
                 <Td>{data?.id}</Td>
-                <Td>{data?.product_title}</Td>
+                <Td>{data?.product_name}</Td>
                 <Td>{data?.amount}</Td>
                 <Td>{data?.shipping_address}</Td>
                 <Td>{data?.created_at}</Td>
