78c284d843cfa7257c9af8b61074489301d835f2
DezyNation <87603422+DezyNation@users.noreply.github.com>
2024-01-11 13:02:00 +0530

    broadcast added

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,25 +1,13 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import {
-  Text,
-  Stack,
-  Box,
-  Image,
-  Button,
-  HStack,
-  VStack,
-  useToast,
-} from "@chakra-ui/react";
+import { Text, Stack, Box, useToast } from "@chakra-ui/react";
 import StatsCard from "@/components/dashboard/StatsCard";
 import {
   BsArrowRight,
   BsCashCoin,
   BsCurrencyRupee,
   BsMegaphoneFill,
-  BsYoutube,
 } from "react-icons/bs";
-import Link from "next/link";
-import { useCookies } from "react-cookie";
 import { isExpired } from "react-jwt";
 import BackendAxios from "@/utils/axios";
 import { MdGroups } from "react-icons/md";
@@ -35,10 +23,8 @@ const DashboardHome = () => {
   const [secondaryJuniors, setSecondaryJuniors] = useState([]);
   const [campaigns, setCampaigns] = useState([]);
   const [collections, setCollections] = useState(null);
-  const [team, setTeam] = useState({
-    primary: [],
-    secondary: [],
-  });
+
+  const [msg, setMsg] = useState("");
 
   useEffect(() => {
     setSessionExpired(isExpired(Cookies.get("jwt")));
@@ -56,6 +42,21 @@ const DashboardHome = () => {
   useEffect(() => {
     fetchPrimaryGroup();
     fetchSecondaryGroup();
+  }, []);
+
+  useEffect(() => {
+    BackendAxios.get(`/api/content`)
+      .then((res) => {
+        const parsedData = JSON.parse(res.data[0]?.content);
+        setMsg(parsedData?.memberBroadcast);
+      })
+      .catch((err) => {
+        Toast({
+          status: "error",
+          description:
+            err?.response?.data?.message || err?.response?.data || err?.message,
+        });
+      });
   }, []);
 
   function fetchCollections() {
@@ -182,123 +183,19 @@ const DashboardHome = () => {
         </Stack>
       )}
 
-      {/* <Box pt={16}>
-        <Stack
-          direction={["column", "row"]}
-          gap={8}
-          justifyContent={"space-between"}
+      {msg ? (
+        <Box
+          mt={16}
+          p={4}
+          bgColor={"yellow.50"}
+          border={"1px solid"}
+          borderColor={"yellow.300"}
+          rounded={4}
         >
-          <Box flex={["unset", 4]}>
-            <Text pb={8}>Your Most Recent Campaign</Text>
-            {campaigns.length > 0 && (
-              <Box
-                pos={"relative"}
-                boxShadow={"md"}
-                rounded={8}
-                overflow={"hidden"}
-                bgImage={
-                  campaigns[0]?.file_path
-                    ? `https://virolife.in/${campaigns[0]?.file_path}`
-                    : "https://idea.batumi.ge/files/default.jpg"
-                }
-                bgSize={"cover"}
-                bgRepeat={"no-repeat"}
-                h={"sm"}
-                cursor={"pointer"}
-              >
-                <Box
-                  position={"absolute"}
-                  w={"full"}
-                  h={"sm"}
-                  top={0}
-                  left={0}
-                  right={0}
-                  bottom={0}
-                  p={4}
-                  bgImage={
-                    "linear-gradient(to top, rgba(0,0,0,0.6), rgba(0,0,0,0.3))"
-                  }
-                  display={"flex"}
-                  flexDirection={"column"}
-                  alignItems={"flex-start"}
-                  justifyContent={"flex-end"}
-                >
-                  <Text pos={"absolute"} top={4} right={4} color={"#FFF"}>
-                    {new Date(campaigns[0]?.updated_at).toDateString()}
-                  </Text>
-                  <Text className="serif" fontSize={"3xl"} color={"#FFF"}>
-                    {campaigns[0]?.title}
-                  </Text>
-                  <Text fontSize={"xs"} color={"#FFF"}>
-                    {campaigns[0]?.description}
-                  </Text>
-                </Box>
-              </Box>
-            )}
-
-            <Link href={"/dashboard/campaigns"}>
-              <Button mt={8} colorScheme="yellow" rightIcon={<BsArrowRight />}>
-                View All Campaigns
-              </Button>
-            </Link>
-          </Box>
-          <Box flex={["unset", 2]}>
-            <Text pb={8}>Your Top Contributors</Text>
-            
-                        <VStack gap={4} w={'full'} p={4} boxShadow={'md'} rounded={8}>
-                            <Box w={'full'} p={2} borderBottom={'1px'} borderColor={'#DDD'}>
-                                <HStack justifyContent={'space-between'} alignItems={'flex-start'}>
-                                    <Box>
-                                        <Text className='serif'>Sangam Kumar</Text>
-                                        <Text fontSize={'xs'} color={'#AAA'}>for Save The Cows</Text>
-                                    </Box>
-                                    <Text fontSize={'sm'} fontWeight={'semibold'}>₹4571</Text>
-                                </HStack>
-                            </Box>
-                            <Box w={'full'} p={2} borderBottom={'1px'} borderColor={'#DDD'}>
-                                <HStack justifyContent={'space-between'} alignItems={'flex-start'}>
-                                    <Box>
-                                        <Text className='serif'>John Doe</Text>
-                                        <Text fontSize={'xs'} color={'#AAA'}>for Save The Cows</Text>
-                                    </Box>
-                                    <Text fontSize={'sm'} fontWeight={'semibold'}>₹3654</Text>
-                                </HStack>
-                            </Box>
-                            <Box w={'full'} p={2} borderBottom={'1px'} borderColor={'#DDD'}>
-                                <HStack justifyContent={'space-between'} alignItems={'flex-start'}>
-                                    <Box>
-                                        <Text className='serif'>Sanjay Mehta</Text>
-                                        <Text fontSize={'xs'} color={'#AAA'}>for Save The Cows</Text>
-                                    </Box>
-                                    <Text fontSize={'sm'} fontWeight={'semibold'}>₹3600</Text>
-                                </HStack>
-                            </Box>
-                            <Box w={'full'} p={2} borderBottom={'1px'} borderColor={'#DDD'}>
-                                <HStack justifyContent={'space-between'} alignItems={'flex-start'}>
-                                    <Box>
-                                        <Text className='serif'>George Bush</Text>
-                                        <Text fontSize={'xs'} color={'#AAA'}>for Save The Cows</Text>
-                                    </Box>
-                                    <Text fontSize={'sm'} fontWeight={'semibold'}>₹2154</Text>
-                                </HStack>
-                            </Box>
-                            <Box w={'full'} p={2} borderBottom={'1px'} borderColor={'#DDD'}>
-                                <HStack justifyContent={'space-between'} alignItems={'flex-start'}>
-                                    <Box>
-                                        <Text className='serif'>Tilak Prasad</Text>
-                                        <Text fontSize={'xs'} color={'#AAA'}>for Save The Cows</Text>
-                                    </Box>
-                                    <Text fontSize={'sm'} fontWeight={'semibold'}>₹3884</Text>
-                                </HStack>
-                            </Box>
-                            <Button pt={4} w={'full'} variant={'link'} colorScheme='yellow'>
-                                View All Contributors
-                            </Button>
-                        </VStack>
-                       
-          </Box>
-        </Stack>
-      </Box> */}
+          <Text fontWeight={"semibold"}>Important Message: </Text>
+          <Text>{msg}</Text>
+        </Box>
+      ) : null}
     </>
   );
 };
