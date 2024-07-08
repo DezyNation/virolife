a45d14d391a23a28f408d083011666590191624e
DezyNation <87603422+DezyNation@users.noreply.github.com>
2024-07-08 20:49:49 +0530

    plans added

---
+++ src/utils/constants.js
@@ -0,0 +1,22 @@
+export const primaryGroupPlans = [
+  {
+    id: "1",
+    label: "A",
+    amount: 25
+  },
+  {
+    id: "2",
+    label: "B",
+    amount: 50
+  },
+  {
+    id: "3",
+    label: "C",
+    amount: 100
+  },
+  {
+    id: "4",
+    label: "D",
+    amount: 200
+  },
+];
